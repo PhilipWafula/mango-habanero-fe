90e4d4b6481de143a69dd089c2ae06ff81bec7b4
PhilipWafula <main@mango-habanero.dev>
2024-06-10 17:20:23 +0300

    chore(maintenance): Adds logo to maintenance card.

--- app/ui/titles.tsx
+++ app/ui/titles.tsx
@@ -1,10 +1,16 @@
 import { Label } from '@/app/ui/typography';
+import Image from 'next/image';
+import mhLogo from '@/public/images/mh-logo.svg';
 
-export function TitleWithLogo({ label, title }: { label: string; title: string }) {
+export function TitleWithLogo({ label, title }: Readonly<{ label: string; title: string }>) {
     return (
         <div className="mb-4 flex flex-row items-center gap-3">
             <div className="flex flex-col items-start">
-                <div className="h-12 w-12 flex-shrink-0 bg-zima-blue"></div>
+                <Image
+                    src={mhLogo}
+                    alt="The mango habanero logo."
+                    className="h-12 w-12 flex-shrink-0"
+                ></Image>
             </div>
             <div className="flex flex-col items-start">
                 <Label label={label} />
